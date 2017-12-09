64fa712720b7bb2e94bbf62a470df632317747fc
HiranyaJathan <hiranya.jathan@gmail.com>
2017-12-09 21:18:10 +0530

    about us and contact us page creation

--- about-us.php
+++ about-us.php
@@ -29,7 +29,112 @@
             </section>
             <div class="clearfix"></div>
             
-            <section id="dummy" style="height: 100vh;">
+            <section id="about-us">
+                <div class="container">
+                    <div class="col-xs-12 col-sm-6">
+                        <div id="carousel-aboutUs" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <img src="http://grandetest.com/theme/agra-html/images/sliders/img05.jpg" alt="Los Angeles" />
+                                </div>
+
+                                <div class="item">
+                                    <img src="http://grandetest.com/theme/agra-html/images/sliders/img05.jpg" alt="Los Angeles" />
+                                </div>
+                            </div>
+                            <i class="carousel-control-left fa fa-angle-left" data-target="#carousel-aboutUs" data-slide="prev"></i>
+                            <i class="carousel-control-right fa fa-angle-right" data-target="#carousel-aboutUs" data-slide="next"></i>
+                        </div>
+                    </div>   
+                    <div class="col-xs-12 col-sm-6 about-text">
+                        <div class="col-xs-12 col-sm-12 p-lr-none">
+                            <h4>Our Vision</h4>
+                            <p>Their house is a museum where people come to see ‘em. They really are a scream the Addams Family. They're creepy and they're kooky mysterious and spooky.</p>
+                            <p>A shadowy flight into the dangerous world of a man who does not exist. Got kind of tired packin' and unpackin town to town and up and down the dial! So join us here each week.</p>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 p-lr-none">
+                            <h4>Our Mission</h4>
+                            <p>Their house is a museum where people come to see ‘em. They really are a scream the Addams Family. They're creepy and they're kooky mysterious and spooky.</p>
+                            <p>A shadowy flight into the dangerous world of a man who does not exist. Got kind of tired packin' and unpackin town to town and up and down the dial! So join us here each week.</p>
+                        </div>
+                    </div>  
+                    <div class="col-xs-12 col-sm-12 about-text">
+                        <h4>Our Mission</h4>
+                        <p>Their house is a museum where people come to see ‘em. They really are a scream the Addams Family. They're creepy and they're kooky mysterious and spooky.</p>
+                        <p>A shadowy flight into the dangerous world of a man who does not exist. Got kind of tired packin' and unpackin town to town and up and down the dial! So join us here each week.</p>
+                    </div>
+                    
+                    <div class="clearfix"></div>
+                    
+                    <div class="col-xs-12 col-sm-12 what-wedo-wrap">
+                        <h1 class="section-title">WHAT WE <strong>DO</strong></h1>
+                        <div id="Carousel-abt-whatwedo" class="carousel slide" data-ride="carousel">
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                              <li data-target="#Carousel-abt-whatwedo" data-slide-to="0" class="active"></li>
+                              <li data-target="#Carousel-abt-whatwedo" data-slide-to="1"></li>
+                              <li data-target="#Carousel-abt-whatwedo" data-slide-to="2"></li>
+                            </ol>
+
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <div class="col-xs-12 col-sm-4">
+                                        <img src="img/about1.jpg" class="img-responsive">
+                                        <div class = "abt-what-text">
+                                            <h3>Green Building</h3>
+                                            <p>Vivamus ac eros at nisl sagittis ornare. Ut ipsum lacus,  Integer vel nisl et tortor gravida pharetra.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-4">
+                                        <img src="img/about2.jpg" class="img-responsive">
+                                        <div class = "abt-what-text">
+                                            <h3>Green Building</h3>
+                                            <p>Vivamus ac eros at nisl sagittis ornare. Ut ipsum lacus,  Integer vel nisl et tortor gravida pharetra.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-4">
+                                        <img src="img/about1.jpg" class="img-responsive">
+                                        <div class = "abt-what-text">
+                                            <h3>Green Building</h3>
+                                            <p>Vivamus ac eros at nisl sagittis ornare. Ut ipsum lacus,  Integer vel nisl et tortor gravida pharetra.</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="item">
+                                     <div class="col-xs-12 col-sm-4">
+                                        <img src="img/about1.jpg" class="img-responsive">
+                                        <div class = "abt-what-text">
+                                            <h3>Green Building</h3>
+                                            <p>Vivamus ac eros at nisl sagittis ornare. Ut ipsum lacus,  Integer vel nisl et tortor gravida pharetra.</p>
+                                        </div>
+                                    </div>
+                                     <div class="col-xs-12 col-sm-4">
+                                        <img src="img/about2.jpg" class="img-responsive">
+                                        <div class = "abt-what-text">
+                                            <h3>Green Building</h3>
+                                            <p>Vivamus ac eros at nisl sagittis ornare. Ut ipsum lacus,  Integer vel nisl et tortor gravida pharetra.</p>
+                                        </div>
+                                    </div>
+                                     <div class="col-xs-12 col-sm-4">
+                                        <img src="img/about1.jpg" class="img-responsive">
+                                        <div class = "abt-what-text">
+                                            <h3>Green Building</h3>
+                                            <p>Vivamus ac eros at nisl sagittis ornare. Ut ipsum lacus,  Integer vel nisl et tortor gravida pharetra.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Left and right controls -->
+                            
+                            <span class="carousel-control-left" data-target="#Carousel-abt-whatwedo" data-slide="prev"><i class="fa fa-long-arrow-left"> </i> PREV</span>
+                            
+                            <span class="carousel-control-right" data-target="#Carousel-abt-whatwedo" data-slide="next">NEXT <i class="fa fa-long-arrow-right"></i></span>
+                        </div>    
+                    </div>  
+                </div>
             </section>
         </main>
 
--- contact-us.php
+++ contact-us.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>About Us | Choice Homes</title>
+    <title>Contact Us | Choice Homes</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -29,7 +29,65 @@
             </section>
             <div class="clearfix"></div>
             
-            <section id="dummy" style="height: 100vh;">
+            <section id="contact-us">                
+                <div class="container">
+                    <div class="col-xs-12 col-sm-12 contact-text-wrap">
+                        <h3>Contact Details</h3>
+                        <p>Their house is a museum where people come to see ‘em. They really are a scream the Addams Family. They're creepy and they're kooky mysterious and spooky. A shadowy flight into the dangerous world of a man who does not exist. Got kind of tired packin' and unpackin town to town and up and down the dial! So join us here each week my friends.</p>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 contact-details-wrap">
+                        <div class="col-xs-12 col-sm-4 text-center">
+                            <div class="contact-detail-border">
+                                <i class="fa fa-user-o" aria-hidden="true"></i>
+                                <h4>Our Location</h4>
+                                <p>145 First Floor Designer Street<br/>
+                                    Newyork, America -2232</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 text-center">
+                            <div class="contact-detail-border">
+                                <i class="fa fa-mobile" aria-hidden="true"></i>
+                                <h4>Call Us</h4>
+                                <p>222 557 661 22<br/>
+                                223 567 89 57<p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 text-center">
+                            <div class="contact-detail-border">
+                                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                <h4>E-MAIL Us</h4>
+                                <a href="mailto:someone@example.com"> <p>info@agra.com</p></a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="clearfix"></div>
+                    
+                    <div class="col-xs-12 col-sm-12 contact-form-wrap">
+                        <h3>Leave Us Your Info</h3>
+                        <form  method="get" action="">
+                            <div class="col-xs-12 col-sm-6 form-group">
+                                <input type="text" class="form-control" id="email" placeholder="Your Name">
+                            </div>
+                            <div class="col-xs-12 col-sm-6 form-group">
+                                <input type="text" class="form-control" id="phone-numb" placeholder="Your Number">
+                            </div>
+                            <div class="col-xs-12 col-sm-12 form-group">
+                                <input type="email" class="form-control" id="email" placeholder="Your Email">
+                            </div>
+                            <div class="col-xs-12 col-sm-12 form-group">
+                                <textarea rows="6" class="form-control" id="msg" placeholder="Your Message "></textarea>
+                            </div>   
+                            <button type="submit" class="btn btn default btn-submit">Submit <i class="fa fa-long-arrow-right"></i></button>
+                        </form>
+                    </div>
+                    
+                    <div class="clearfix"></div>
+                    
+                    <div class="col-xs-12 col-sm-12 map-wrap">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.479687937281!2d72.88678531490136!3d19.08660098708182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c887efb78b9f%3A0x9f9dc99c3119470a!2sPhoenix+Marketcity!5e0!3m2!1sen!2sin!4v1512830020303" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                </div>
             </section>
         </main>
 
